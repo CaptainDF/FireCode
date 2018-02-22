e44555658e13b1fa848d8285684c09b2103d8d06
duanzhiang <123802663@qq.com>
2018-02-22 17:54:05 +0800

    使用coolections接口中的sort方法进行排序， 重写compare方法，比较map中的value

--- src/com/fire/quiz4/PriceSort.java
+++ src/com/fire/quiz4/PriceSort.java
@@ -3,8 +3,7 @@ package com.fire.quiz4;
 import org.junit.Test;
 
 import java.io.*;
-import java.util.Set;
-import java.util.TreeMap;
+import java.util.*;
 
 public class PriceSort {
 
@@ -15,12 +14,12 @@ public class PriceSort {
         BufferedReader reader = new BufferedReader(
                 new InputStreamReader(is));
         String line;
-        TreeMap sortedProduct = new TreeMap<Integer,String>();
+        Map sortedProduct = new HashMap<String,Integer>();
         while(reader.ready()){
             line = reader.readLine();
             //System.out.println(line);
             String[] s = line.split(",");
-            sortedProduct.put(s[1],s[0]);
+            sortedProduct.put(s[0],s[1]);
         }
         reader.close();
         is.close();
@@ -29,10 +28,28 @@ public class PriceSort {
         FileWriter fw =  new FileWriter("Sorted.txt");
         BufferedWriter bufw = new BufferedWriter(fw);
 
-        //遍历排序后的map
-        Set treeSet = sortedProduct.keySet();
-        for(Object i:treeSet){
-            bufw.write(sortedProduct.get(i)+","+i);
+        List<Map.Entry<String, Integer>> list1 = new ArrayList<Map.Entry<String, Integer>>(sortedProduct.entrySet());
+        Collections.sort(list1, new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                int value1 = Integer.parseInt(o1.getValue()+"");
+//                System.out.println(value1);
+                int value2 = Integer.parseInt(o2.getValue()+"");
+                if(value1>value2){
+                    return 1;
+                }
+                if(value1<value2){
+                    return -1;
+                }
+                return 0;
+            }
+        });
+
+
+        //遍历排序后的list1
+        //Set treeSet = sortedProduct.keySet();
+        for(Map.Entry<String, Integer> entry : list1){
+            bufw.write(entry.getKey()+","+entry.getValue());
             bufw.newLine();
             //System.out.println("key="+i+",,,value="+sortedProduct.get(i));
         }
